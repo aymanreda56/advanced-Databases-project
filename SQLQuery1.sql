693379d7f2ae02b5df9399931d963a74ce9de19a
aymanreda56 <naynoona111@yahoo.com>
2022-12-14 20:06:58 +0200

    added scripts for filling other tables

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -19,21 +19,6 @@ Sex tinyint check(Sex between 0 and 2),
 Salary int
 );
 
-create table Intern
-(
-SSN int identity (3000000,1) primary key Not NULL,
-Fname nvarchar(50),
-Minit varchar,
-Lname nvarchar(50),
-Bdate DATE,
-Address_ nvarchar(50),
-Sex tinyint check(Sex between 0 and 2),
-Salary int,
-Major nvarchar(50),
-PNO INT foreign key references Project(Pnumber),
-Super_SSN int foreign key references Employee(SSN)
-);
-
 
 create table Department
 (
@@ -52,13 +37,6 @@ alter table Employee
 add Super_SSN int foreign key references Employee(SSN);
 
 
-create table Dept_Locations
-(
-Dnumber INT foreign key references Department(Dnumber),
-Dlocation nvarchar(50) NOT NULL,
-CONSTRAINT Department_KEY_COMPOSITE PRIMARY KEY (Dnumber, Dlocation)
-);
-
 create table Project
 (
 Pname nvarchar(50),
@@ -68,6 +46,52 @@ DNO INT foreign key references Department(Dnumber),
 CONSTRAINT Project_KEY_COMPOSITE PRIMARY KEY (Pnumber, Plocation)
 );
 
+--select * from Project where (Pnumber = 10);
+
+create table Intern
+(
+SSN bigint identity (3000000,1) primary key Not NULL,
+Fname nvarchar(50),
+Minit varchar,
+Lname nvarchar(50),
+Bdate DATE,
+Address_ nvarchar(50),
+Sex tinyint check(Sex between 0 and 2),
+Salary int,
+Major nvarchar(50),
+PNO int foreign key references Project(Pnumber),
+Super_SSN int foreign key references Employee(SSN)
+);
+
+--select count(*) from Project;
+--delete from Contracts;
+
+create table Contracts
+(
+Name_Company nvarchar(50),
+Date_created DATE,
+ProjectID int,
+Product nvarchar(50),
+Price int,
+Date_of_Contract DATE,
+Emp_Name nvarchar(50),
+Emp_SSN int,
+Contractor_Name nvarchar(50),
+Cont_SSN int,
+Delivery_Location nvarchar(50),
+CONSTRAINT Contracts_KEY_COMPOSITE PRIMARY KEY (Product, Emp_SSN, Cont_SSN)
+);
+
+
+drop table Dept_Locations;
+
+create table Dept_Locations
+(
+Dnumber INT foreign key references Department(Dnumber),
+Dlocation nvarchar(50) NOT NULL,
+CONSTRAINT Department_KEY_COMPOSITE PRIMARY KEY (Dnumber, Dlocation)
+);
+select * from Dept_Locations;
 
 create table Works_On
 (
@@ -90,3 +114,4 @@ CONSTRAINT Dependent_KEY_COMPOSITE PRIMARY KEY (ESSN, Dependent_Name)
 
 
 INSERT INTO Employee(Fname, Minit, Lname, Bdate, Address_, Sex, Salary) Values ('ayman','m','MOh', '11/11/2000', 'ahmed', 2, 3000);
+select count (*) from Employee
